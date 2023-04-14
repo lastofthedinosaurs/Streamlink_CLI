c40de52ca25e4971ba52fa65c2e365d898aee3a7
Last <noreply@gitlab.lab.local>
2023-04-14 06:06:01 -0500

    Update pylint.yml

--- src/streamlink_cli.py
+++ src/streamlink_cli.py
@@ -14,30 +14,30 @@ def player_log(loglevel, component, message):
     print(f"[{loglevel}] {component}: {message}")
 
 
-player = mpv.MPV(log_handler=player_log, ytdl=False, input_default_bindings=True, input_vo_keyboard=True)
+player = mpv.MPV(
+    log_handler=player_log,
+    ytdl=False,
+    input_default_bindings=True,
+    input_vo_keyboard=True)
 player.fullscreen = False
 player.loop_playlist = "inf"
 player.sid = "auto"
 player.hwaccel = "auto"
-
-# Option access, in general these require the core to reinitialize
 player["stop-screensaver"] = "yes"
-
-# Specify a priority list of audio/video output drivers to be used.
-# If the list has a trailing ",", mpv will fall back on drivers not contained in the list.
 player["vo"] = "gpu,"
 player["ao"] = "alsa,"
+
+
+def check(evt):
+    toks = evt["event"]["text"].split()
+    if "silence_end:" in toks:
+        return float(toks[2])
 
 
 def skip_silence():
     player.set_loglevel("warn")
     player.af = "lavfi=[silencedetect=n=-20dB:d=1]"
     player.speed = 100
-
-    def check(evt):
-        toks = evt["event"]["text"].split()
-        if "silence_end:" in toks:
-            return float(toks[2])
 
     try:
         player.time_pos = player.wait_for_event("log_message", cond=check)
