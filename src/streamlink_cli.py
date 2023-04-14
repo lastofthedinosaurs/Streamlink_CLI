e99da34be49ba4267b9c4f87d43a099ca100707c
Last <noreply@gitlab.lab.local>
2023-04-14 11:50:29 -0500

    Added docs directory

--- src/streamlink_cli.py
+++ src/streamlink_cli.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/env
+"""
+Receives input data from various APIs (Twitch, YouTube, etc.) before
+piping it to mpv for video playback. Other streaming/video platforms
+can easily be added through the use of Streamlink Plugins.
+"""
 
 import mpv
 from streamlink import Streamlink
@@ -11,6 +16,7 @@ stream = session.streams("https://www.twitch.tv/<REDACTED>")
 
 
 def player_log(loglevel, component, message):
+    """ Streamlink logger """
     print(f"[{loglevel}] {component}: {message}")
 
 
@@ -18,7 +24,8 @@ player = mpv.MPV(
     log_handler=player_log,
     ytdl=False,
     input_default_bindings=True,
-    input_vo_keyboard=True)
+    input_vo_keyboard=True
+)
 player.fullscreen = False
 player.loop_playlist = "inf"
 player.sid = "auto"
@@ -28,16 +35,18 @@ player["vo"] = "gpu,"
 player["ao"] = "alsa,"
 
 
-def check(evt):
-    toks = evt["event"]["text"].split()
-    if "silence_end:" in toks:
-        return float(toks[2])
-
-
 def skip_silence():
+    """
+    Used to automatically skip muted segments in Twitch VODs
+    """
     player.set_loglevel("warn")
     player.af = "lavfi=[silencedetect=n=-20dB:d=1]"
     player.speed = 100
+
+    def check(evt):
+        toks = evt["event"]["text"].split()
+        if "silence_end:" in toks:
+            return float(toks[2])
 
     try:
         player.time_pos = player.wait_for_event("log_message", cond=check)
@@ -50,6 +59,7 @@ def skip_silence():
 
 @player.python_stream("streamlink-cli")
 def reader(quality="best"):
+    """ Open stream URL as a file """
     with stream[quality].open() as f:
         while True:
             yield f.read(1024*1024)
@@ -58,8 +68,10 @@ def reader(quality="best"):
 # Property access, these can be changed at runtime
 @player.property_observer("time-pos")
 def time_observer(_name, value):
-    # Here, _value is either None if nothing is playing or a float containing
-    # fractional seconds since the beginning of the file.
+    """
+    Here, value is either None if nothing is playing or a float containing
+    fractional seconds since the beginning of the file.
+    """
     try:
         print(f"Now playing at {value:.2f}s")
     except TypeError:
@@ -68,17 +80,20 @@ def time_observer(_name, value):
 
 @player.on_key_press("q")
 def q_binding():
+    """ MPV keyboard binding """
     print("THERE IS NO ESCAPE")
 
 
 @player.on_key_press("s")
 def s_binding():
+    """ MPV keyboard binding """
     i = player.screenshot_raw()
     i.save("screenshot.png")
 
 
 if __name__ == "__main__":
     player.play("python://streamlink-cli")
+    skip_silence()
     player.wait_for_playback()
 
     del player
--- src/twitch.py
+++ src/twitch.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python3
+"""
+Used to query the Twitch API for data to feed to Streamlink
+"""
 
 import requests
 
-CLIENT_SECRET = "<REDACTED>"
-CLIENT_ID = "<REDACTED>"
-STREAMER = "<REDACTED>"
+CLIENT_SECRET = '<REDACTED>'
+CLIENT_ID = '<REDACTED>'
+STREAMER = '<REDACTED>'
 
 
 def get_access_token():
+    """
+    Twitch APIs require OAuth 2.0 access tokens to access resources.
+    """
     with requests.Session() as s:
         body = {
-            "CLIENT_ID": CLIENT_ID,
-            "CLIENT_SECRET": CLIENT_SECRET,
+            "client_id": CLIENT_ID,
+            "client_secret": CLIENT_SECRET,
             "grant_type": "client_credentials"
         }
         r = s.post('https://id.twitch.tv/oauth2/token', body)
@@ -19,18 +25,24 @@ def get_access_token():
         return r.json()
 
 
-def get_stream_data(k):
+def api_request(k, u, q=''):
+    """
+    Send a GET request to the Twitch API
+    """
     with requests.Session() as s:
         headers = {
             "Client-ID": CLIENT_ID,
             "Authorization": f"Bearer {k['access_token']}"
         }
-        r = s.get(f'https://api.twitch.tv/helix/streams?user_login={STREAMER}', headers=headers)
+        r = s.get(f"{u}?{q}", headers=headers)
         r.raise_for_status()
         return r.json()
 
 
 def print_now_playing(d):
+    """
+    Print information about the requested stream
+    """
     if len(d['data']) == 1:
         title = d['data'][0]['title']
         game = d['data'][0]['game_name']
@@ -42,6 +54,10 @@ def print_now_playing(d):
 
 if __name__ == '__main__':
     keys = get_access_token()
-    data = get_stream_data(keys)
+
+    url = "https://api.twitch.tv/helix/streams"
+    query = f"user_login={STREAMER}"
+
+    data = api_request(keys, url, query)
 
     print_now_playing(data)
