b30fdae07bab19d12c4d3ff3649e3d971d8bdf80
Last <noreply@gitlab.lab.local>
2023-04-14 05:28:06 -0500

    Update pylint.yml

--- src/streamlink-cli.py
+++ src/streamlink-cli.py
@@ -11,12 +11,12 @@ stream = session.streams("https://www.twitch.tv/<REDACTED>")
 
 
 def player_log(loglevel, component, message):
-    print('[{}] {}: {}'.format(loglevel, component, message))
+    print(f"[{loglevel}] {component}: {message}")
 
 
 player = mpv.MPV(log_handler=player_log, ytdl=False, input_default_bindings=True, input_vo_keyboard=True)
 player.fullscreen = False
-player.loop_playlist = 'inf'
+player.loop_playlist = "inf"
 player.sid = "auto"
 player.hwaccel = "auto"
 
@@ -24,29 +24,29 @@ player.hwaccel = "auto"
 player["stop-screensaver"] = "yes"
 
 # Specify a priority list of audio/video output drivers to be used.
-# If the list has a trailing ',', mpv will fall back on drivers not contained in the list.
-player['vo'] = 'gpu,'
-player['ao'] = 'alsa,'
+# If the list has a trailing ",", mpv will fall back on drivers not contained in the list.
+player["vo"] = "gpu,"
+player["ao"] = "alsa,"
 
 
 def skip_silence():
-    player.set_loglevel('warn')
-    player.af = 'lavfi=[silencedetect=n=-20dB:d=1]'
+    player.set_loglevel("warn")
+    player.af = "lavfi=[silencedetect=n=-20dB:d=1]"
     player.speed = 100
 
     def check(evt):
-        toks = evt['event']['text'].split()
-        if 'silence_end:' in toks:
+        toks = evt["event"]["text"].split()
+        if "silence_end:" in toks:
             return float(toks[2])
 
     try:
-        player.time_pos = player.wait_for_event('log_message', cond=check)
+        player.time_pos = player.wait_for_event("log_message", cond=check)
     except TypeError as e:
         print(e)
         pass
 
     player.speed = 1
-    player.af = ''
+    player.af = ""
 
 
 @player.python_stream("streamlink-cli")
@@ -57,28 +57,28 @@ def reader(quality="best"):
 
 
 # Property access, these can be changed at runtime
-@player.property_observer('time-pos')
+@player.property_observer("time-pos")
 def time_observer(_name, value):
     # Here, _value is either None if nothing is playing or a float containing
     # fractional seconds since the beginning of the file.
     try:
-        print('Now playing at {:.2f}s'.format(value))
+        print(f"Now playing at {value:.2f}s")
     except TypeError:
         pass
 
 
-@player.on_key_press('q')
+@player.on_key_press("q")
 def q_binding():
-    print('THERE IS NO ESCAPE')
+    print("THERE IS NO ESCAPE")
 
 
-@player.on_key_press('s')
+@player.on_key_press("s")
 def s_binding():
-    img = player.screenshot_raw()
-    img.save('screenshot.png')
+    i = player.screenshot_raw()
+    i.save("screenshot.png")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     player.play("python://streamlink-cli")
     player.wait_for_playback()
 
