d46817c5e977953b59cf075439b764ee848b02c6
Last <noreply@gitlab.lab.local>
2023-04-14 12:16:34 -0500

    Variable name changes

--- src/twitch.py
+++ src/twitch.py
@@ -5,54 +5,54 @@ Used to query the Twitch API for data to feed to Streamlink
 
 import requests
 
-CLIENT_SECRET = '<REDACTED>'
-CLIENT_ID = '<REDACTED>'
-STREAMER = '<REDACTED>'
+CLIENT_SECRET = "<REDACTED>"
+CLIENT_ID = "<REDACTED>"
+STREAMER ="<REDACTED>"
 
 
 def get_access_token():
     """
     Twitch APIs require OAuth 2.0 access tokens to access resources.
     """
-    with requests.Session() as s:
+    with requests.Session() as session:
         body = {
             "client_id": CLIENT_ID,
             "client_secret": CLIENT_SECRET,
             "grant_type": "client_credentials"
         }
-        r = s.post('https://id.twitch.tv/oauth2/token', body)
-        r.raise_for_status()
-        return r.json()
+        response = session.post("https://id.twitch.tv/oauth2/token", body)
+        response.raise_for_status()
+        return response.json()
 
 
 def api_request(keys, url, query=''):
     """
     Send a GET request to the Twitch API
     """
-    with requests.Session() as s:
+    with requests.Session() as session:
         headers = {
             "Client-ID": CLIENT_ID,
             "Authorization": f"Bearer {keys['access_token']}"
         }
-        r = s.get(f"{url}?{query}", headers=headers)
-        r.raise_for_status()
-        return r.json()
+        response = session.get(f"{url}?{query}", headers=headers)
+        response.raise_for_status()
+        return response.json()
 
 
 def print_now_playing(data):
     """
     Print information about the requested stream
     """
-    if len(data['data']) == 1:
-        title = data['data'][0]['title']
-        game = data['data'][0]['game_name']
-        # started_at = data['data'][0]['started_at']
+    if len(data["data"]) == 1:
+        title = data["data"][0]["title"]
+        game = data["data"][0]["game_name"]
+        # started_at = data["data"][0]["started_at"]
         print(f"{STREAMER} - [{game}] : {title}")
     else:
         print(f"{STREAMER} is not live")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     KEYS = get_access_token()
 
     URL = "https://api.twitch.tv/helix/streams"
