26ed07797100b6ee9ee5015ff80c30f8e68670ca
Last <noreply@gitlab.lab.local>
2023-04-14 05:44:25 -0500

    Update pylint.yml

--- src/twitch.py
+++ src/twitch.py
@@ -2,16 +2,16 @@
 
 import requests
 
-client_secret = "<REDACTED>"
-client_id = "<REDACTED>"
-streamer = "<REDACTED>"
+CLIENT_SECRET = "<REDACTED>"
+CLIENT_ID = "<REDACTED>"
+STREAMER = "<REDACTED>"
 
 
 def get_access_token():
     with requests.Session() as s:
         body = {
-            "client_id": client_id,
-            "client_secret": client_secret,
+            "CLIENT_ID": CLIENT_ID,
+            "CLIENT_SECRET": CLIENT_SECRET,
             "grant_type": "client_credentials"
         }
         r = s.post('https://id.twitch.tv/oauth2/token', body)
@@ -22,10 +22,10 @@ def get_access_token():
 def get_stream_data(k):
     with requests.Session() as s:
         headers = {
-            "Client-ID": client_id,
+            "Client-ID": CLIENT_ID,
             "Authorization": f"Bearer {k['access_token']}"
         }
-        r = s.get(f'https://api.twitch.tv/helix/streams?user_login={streamer}', headers=headers)
+        r = s.get(f'https://api.twitch.tv/helix/streams?user_login={STREAMER}', headers=headers)
         r.raise_for_status()
         return r.json()
 
@@ -35,9 +35,9 @@ def print_now_playing(d):
         title = d['data'][0]['title']
         game = d['data'][0]['game_name']
         started_at = d['data'][0]['started_at']
-        print(f"{streamer} - [{game}] : {title}")
+        print(f"{STREAMER} - [{game}] : {title}")
     else:
-        print(f"{streamer} is not live")
+        print(f"{STREAMER} is not live")
 
 
 if __name__ == '__main__':
