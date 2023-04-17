18474e41b32512a9e53efd1fc1a2b963908e8fc3
Last <noreply@gitlab.lab.local>
2023-04-17 15:56:43 -0500

    Update class APIhelper

--- src/twitch.py
+++ src/twitch.py
@@ -18,13 +18,79 @@ class Config:
 
 class APIhelper:
     id = ""
+    name = ""
     login = ""
     user_id = ""
     user_login = ""
     game_id = ""
+    broadcaster_id = ""
     type = "live"
     lang = "en"
     first = 10
+    started_at = ""
+    ended_at = ""
+    igdb_id = ""
+    period = ""
+
+    def get_channel_stream_schedule(self, keys, query=""):
+        """
+        Gets the broadcaster’s streaming schedule. You can get the entire
+        schedule or specific segments of the schedule.
+        """
+        url = "https://api.twitch.tv/helix/schedule"
+        if not query:
+            if self.broadcaster_id:
+                query = f"broadcaster_id={self.broadcaster_id}"
+            elif self.id:
+                query = f"id={self.id}"
+        return api_request(keys, url, f"{query}")
+
+    def get_clips(self, keys, query=""):
+        """
+        Gets one or more video clips that were captured from streams.
+        """
+        url = "https://api.twitch.tv/helix/clips"
+        if not query:
+            if self.broadcaster_id:
+                query = f"broadcaster_id={self.broadcaster_id}"
+            elif self.game_id:
+                query = f"game_id={self.game_id}"
+            elif self.id:
+                query = f"id={self.id}"
+        return api_request(keys, url, f"{query}")
+
+    def get_followed_channels(self, keys, query=""):
+        """
+        Gets a list of broadcasters that the specified user follows. 
+        You can also use this endpoint to see whether a user follows 
+        a specific broadcaster.
+        """
+        url = "https://api.twitch.tv/helix/followed"
+        if not query:
+            if self.user_id:
+                query = f"user_id={self.user_id}"
+            elif self.broadcaster_id:
+                query = f"broadcaster_id={self.broadcaster_id}"
+        return api_request(keys, url, f"{query}")
+
+    def get_games(self, keys, query=""):
+        """
+        Gets information about specified categories or games.
+
+        You may get up to 100 categories or games by specifying their ID 
+        or name. You may specify all IDs, all names, or a combination of 
+        IDs and names. If you specify a combination of IDs and names, the 
+        total number of IDs and names must not exceed 100.
+        """
+        url = "https://api.twitch.tv/helix/games"
+        if not query:
+            if self.id:
+                query = f"id={self.id}"
+            elif self.name:
+                query = f"name={self.name}"
+            elif self.igdb_id:
+                query = f"igdb_id={self.igdb_id}"
+        return api_request(keys, url, f"{query}")
 
     def get_streams(self, keys, query=""):
         """
@@ -43,11 +109,21 @@ class APIhelper:
                 query = f"user_login={self.user_login}"
             elif self.game_id:
                 query = f"game_id={self.game_id}"
-        return api_request(
-            keys,
-            url,
-            f"{query}&language={self.lang}".strip()
-        )
+        # Append options to query
+        query = f"{query}&type={self.type}"
+        query = f"{query}&language={self.lang}"
+        query = f"{query}&first={self.first}"
+        return api_request(keys, url, f"{query}")
+
+    def get_top_games(self, keys, query=""):
+        """
+        Gets information about all broadcasts on Twitch.
+        """
+        url = "https://api.twitch.tv/helix/games/top"
+        if not query:
+            if self.first:
+                query = f"first={self.first}"
+        return api_request(keys, url, f"{query}")
 
     def get_users(self, keys, query=""):
         """
@@ -64,6 +140,34 @@ class APIhelper:
             elif self.login:
                 query = f"login={self.login}"
         return api_request(keys, url, query)
+
+    def get_videos(self, keys, query=""):
+        """
+        Gets information about one or more published videos. You may 
+        get videos by ID, by user, or by game/category.
+
+        You may apply several filters to get a subset of the videos.
+        The filters are applied as an AND operation to each video.
+        For example, if language is set to ‘de’ and game_id is set to
+        21779, the response includes only videos that show playing
+        League of Legends by users that stream in German. The filters
+        apply only if you get videos by user ID or game ID.
+        """
+        url = "https://api.twitch.tv/helix/videos"
+        if not query:
+            if self.id:
+                query = f"id={self.id}"
+            elif self.user_id:
+                query = f"user_id={self.user_id}"
+            elif self.game_id:
+                query = f"game_id={self.game_id}"
+        # Append options to query
+        query = f"{query}&type={self.type}"
+        query = f"{query}&language={self.lang}"
+        query = f"{query}&first={self.first}"
+        if self.period:
+            query = f"{query}&period={self.period}"
+        return api_request(keys, url, f"{query}")
 
 
 def get_access_token():
@@ -91,7 +195,7 @@ def api_request(keys, url, query=""):
             "Client-ID": CONFIG.CLIENT_ID,
             "Authorization": f"Bearer {keys['access_token']}"
         }
-        response = session.get(f"{url}?{query}", headers=headers)
+        response = session.get(f"{url}?{query}".strip(), headers=headers)
         response.raise_for_status()
         return response.json()
 
